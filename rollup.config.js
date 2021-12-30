ff89863e34bf02f105d39ed13fb841436f024a7b
DeFizerYield <78314301+Y0moo@users.noreply.github.com>
2021-12-30 19:13:55 +0700

    output build fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
+import url from "@rollup/plugin-url";
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.ts',
-  output: {
-    dir: 'output',
-    format: 'cjs'
-  },
-  plugins: [typescript()]
+  input: "src/index.ts",
+  output: [
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "es" },
+  ],
+  plugins: [url(), typescript()],
 };

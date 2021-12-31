20ff75f2c13505bac1587edd77c4343d2316ae3b
BillyG83 <bgilmore83@hotmail.co.uk>
2021-12-31 11:50:50 +0100

    chore(BannerStrip): reformatting test file

--- src/components/BannerStrip/BannerStrip.test.tsx
+++ src/components/BannerStrip/BannerStrip.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
 import { composeStories } from "@storybook/testing-react";
 import * as stories from "./BannerStrip.stories";

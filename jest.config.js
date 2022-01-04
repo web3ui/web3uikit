e16820424bf356eb665e79f792be0c153af51657
oscario2 <oscarandersson550@gmail.com>
2022-01-04 14:00:41 +0100

    feat: add credit card

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ const { paths } = require('tsdx/dist/constants');
 
 const config = createJestConfig(undefined, paths.appRoot);
 config.transform['^.+\\.svg$'] = 'jest-svg-transformer';
+config.globals = {
+  'ts-jest': {
+    isolatedModules: true,
+  },
+};
 module.exports = config;

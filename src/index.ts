0a4523ae40f5d2a7f4b682427130108369a04d49
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-05 23:12:14 +0100

    fix(index): missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 // Components
-export * from "./components/Button";
 export * from "./components/BannerStrip";
+export * from "./components/Breadcrumbs";
+export * from "./components/Button";
 export * from "./components/Checkbox";
 export * from "./components/Icon";
 export * from "./components/Input";
 export * from "./components/LinkTo";
+export * from "./components/Notification";
+export * from "./components/Radios";
 export * from "./components/Tag";
 export * from "./components/Tooltip";

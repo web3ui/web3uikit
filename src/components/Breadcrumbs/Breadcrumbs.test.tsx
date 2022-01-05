f2cb0b607e9486f08df3844cd727c161b5192f47
DeFizerYield <78314301+Y0moo@users.noreply.github.com>
2022-01-05 22:48:19 +0700

    format

--- src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -9,53 +9,59 @@ import "jest-styled-components";
 const { One } = composeStories(stories);
 
 describe("Breadcrumbs - One", () => {
-    let container: HTMLDivElement;
-    let olId = "breadcrumbs-ol-test-id";
-    let navId = "breadcrumbs-nav-test-id";
-    let separatorId = "breadcrumbs-separator-test-id";
-    let breadcrumbId = "breadcrumb-test-id";
+        let container: HTMLDivElement;
+        let olId = "breadcrumbs-ol-test-id";
+        let navId = "breadcrumbs-nav-test-id";
+        let separatorId = "breadcrumbs-separator-test-id";
+        let breadcrumbId = "breadcrumb-test-id";
 
-    beforeEach(() => {
-        container = document.createElement("div");
-        document.body.appendChild(container);
-        ReactDOM.render(<One />, container);
-    });
+        beforeEach(() => {
+                container = document.createElement("div");
+                document.body.appendChild(container);
+                ReactDOM.render(<One />, container);
+        });
 
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
+        afterEach(() => {
+                document.body.removeChild(container);
+                container.remove();
+        });
 
-    it("nav wrapper should  be visible", () => {
-        const element = container.querySelector(`[data-testid="${navId}"]`);
-        expect(element).not.toBeNull();
-    });
+        it("nav wrapper should  be visible", () => {
+                const element = container.querySelector(
+                        `[data-testid="${navId}"]`,
+                );
+                expect(element).not.toBeNull();
+        });
 
-    it("separator should be not visible", () => {
-        const element = container.querySelector(
-            `[data-testid="${separatorId}"]`,
-        );
-        expect(element).toBeNull();
-    });
+        it("separator should be not visible", () => {
+                const element = container.querySelector(
+                        `[data-testid="${separatorId}"]`,
+                );
+                expect(element).toBeNull();
+        });
 
-    it("svg icon should be visible", () => {
-        const element = container
-            .querySelector(`[data-testid="${breadcrumbId}"]`)
-            ?.querySelector("svg");
-        expect(element).toBeDefined();
-    });
+        it("svg icon should be visible", () => {
+                const element = container
+                        .querySelector(`[data-testid="${breadcrumbId}"]`)
+                        ?.querySelector("svg");
+                expect(element).toBeDefined();
+        });
 
-    it("ol should be visible", () => {
-        const element = container.querySelector(`[data-testid="${olId}"]`);
-        expect(element).not.toBeNull();
-    });
+        it("ol should be visible", () => {
+                const element = container.querySelector(
+                        `[data-testid="${olId}"]`,
+                );
+                expect(element).not.toBeNull();
+        });
 
-    it("breadcrumb should not change color on hover", async () => {
-        fireEvent.mouseOver(screen.getByTestId(breadcrumbId));
-        await waitFor(() => screen.getByTestId(breadcrumbId));
+        it("breadcrumb should not change color on hover", async () => {
+                fireEvent.mouseOver(screen.getByTestId(breadcrumbId));
+                await waitFor(() => screen.getByTestId(breadcrumbId));
 
-        expect(
-            container.querySelector(`[data-testid="${breadcrumbId}"]`),
-        ).toHaveStyleRule(`background: ${color.grey}`);
-    });
+                expect(
+                        container.querySelector(
+                                `[data-testid="${breadcrumbId}"]`,
+                        ),
+                ).toHaveStyleRule(`background: ${color.grey}`);
+        });
 });

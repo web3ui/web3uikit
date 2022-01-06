41085ca2a8d65645c470d4252d4c421ddbf2e08f
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-06 09:11:58 +0100

    fix(Modal): types
    id is optional now.
    reordering alphabetically

--- src/components/Modal/types.ts
+++ src/components/Modal/types.ts
@@ -1,25 +1,15 @@
 export interface ModalProps {
 
     /**
-     * this collection of radio buttons needs an ID to group them
+     * The ID of the modal will be generated automatically if not set
      */
-    id: string;
+    id?: string;
 
     /**
      * set content of modal
      */
     children: Array<React.ReactNode>;
-
-    /*
-    *  set text of 'Ok' button
-    */
-    okText?: string;
-
-    /**
-     * set if 'Ok' button is disabled
-     */
-    okDisabled?: boolean;
-
+    
     /**
      * set if 'Cancel' button is disabled
      */
@@ -29,22 +19,31 @@ export interface ModalProps {
      * set text of 'Cancel' button
      */
     cancelText?: string;
-
+    
     /**
      * set if modal is visible
      */
     isVisible: boolean;
-
+    
+     /**
+     * Run function on 'Cancel'
+     */
+    onCancel?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    
+    /**
+     * set if 'Ok' button is disabled
+     */
+    okDisabled?: boolean;
+    
     /**
      * Run function on 'Ok'
      */
     onOk: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 
-    /**
-     * Run function on 'Cancel'
-     */
-    onCancel?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-
+    /*
+    *  set text of 'Ok' button
+    */
+    okText?: string;
 
     /**
      * set title of modal

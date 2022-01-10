29dec496092c0e364d7ca5da09da1c50058c119f
Y0moo <78314301+Y0moo@users.noreply.github.com>
2022-01-10 20:08:48 +0700

    feat: resetButtonCSS added

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -7,4 +7,14 @@ const resetCSS = `
   padding: 0;
 `;
 
+export const resetButtonCSS = `
+  background: none;
+	border: none;
+	color: inherit;
+	cursor: pointer;
+	font: inherit;
+	outline: inherit;
+	padding: 0;
+`;
+
 export default resetCSS;

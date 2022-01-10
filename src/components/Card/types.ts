0c388f0d1a41ac87b6bc2491efeceb3616643a24
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-10 10:49:38 +0100

    refactor(Card): added type comments

--- src/components/Card/types.ts
+++ src/components/Card/types.ts
@@ -1,8 +1,32 @@
 export interface CardProps {
+    
+    /**
+     * id will be generated automatically if not set
+     */
     id?: string;
+
+    /**
+     * set the content of the card
+     */
     children: Array<React.ReactNode>;
+
+    /**
+     * set the description of the card
+     */
     description?: string;
+
+    /**
+     * set if card is selected
+     */
     selected?: boolean;
+
+    /**
+     * set title of card
+     */
     title?: string;
+
+    /**
+     * set text inside tooltip
+     */
     tooltipText?: string;
 }

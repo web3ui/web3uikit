a4cba24c4f7a8d17157c1efe402c7474a5cd95fc
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-10 16:29:13 +0100

    feat(Widget): init commit

---
+++ src/components/Widget/Widget.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import Widget from './Widget';
+import {iconTypes} from "../Icon/collection";
+
+export default {
+    title: 'UI/Widget',
+    component: Widget,
+} as ComponentMeta<typeof Widget>;
+
+const Template: ComponentStory<typeof Widget> = (args) => <Widget {...args} />
+;
+
+export const TextOnly = Template.bind({});
+TextOnly.args = {
+    title: "Environment",
+    description: "54.324 Kb",
+};
+export const TextOnlyWithHint = Template.bind({});
+TextOnlyWithHint.args = {
+    title: "Environment",
+    description: "4.325 cu",
+    hint: "Thanks for checking me"
+};
+
+export const leadingIcon = Template.bind({});
+leadingIcon.args = {
+    title: "Environment",
+    description: "4.325 cu",
+    hint: "Thanks for checking me",
+    icon: iconTypes.network,
+    iconLayout: "leading",
+};
---
+++ src/components/Widget/Widget.tsx
@@ -0,0 +1,37 @@
+import {WidgetProps} from "./types";
+import React from "react";
+import { HintStyled, WidgetStyled, WidgetInfoBox, WidgetTitle, WidgetInfo } from "./Widget.style";
+import colors from "../../styles/colors";
+import {Icon} from "../Icon";
+import {Tooltip} from "../Tooltip";
+import {iconTypes} from "../Icon/collection";
+
+const getHint = (iconLayout: "leading" | "trailing" | "none", hint: string) => {
+    return (
+        <HintStyled iconLayout={iconLayout}>
+            <Tooltip text={hint} children={[<Icon fill={colors.blue} size={14} svg={iconTypes.helpCircle}/>]} />
+        </HintStyled>
+    )
+}
+
+const Widget: React.FC<WidgetProps> = ({
+    id,
+    description,
+    hint,
+    icon,
+    iconLayout,
+    title
+}: WidgetProps) => {
+    return (
+        <WidgetStyled iconLayout={iconLayout} id={id}>
+            <WidgetInfoBox>
+                <WidgetTitle>{title}</WidgetTitle>
+                <WidgetInfo>{description}</WidgetInfo>
+            </WidgetInfoBox>
+            {hint && getHint(iconLayout, hint)}
+            {icon && <div style={{display: "grid", placeItems: "center", marginRight: '10px'}}><Icon size={30} fill={colors.blue} svg={icon}/></div>}
+        </WidgetStyled>
+    )
+}
+
+export default Widget

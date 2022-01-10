62febf1cf5a65557716af360837362ebff80edd6
Loco The Dev <89942527+locothedev@users.noreply.github.com>
2022-01-10 18:27:10 +0100

    refactor(Illustrations): types

--- src/components/Illustrations/types.ts
+++ src/components/Illustrations/types.ts
@@ -1,9 +1,22 @@
+export const sizeState = ["xs", "s", "m", "l", "xl"] as const;
+export type size = typeof sizeState[number];
+
+export const chainState = ["ethereum" , "binance" , "polygon" , "arbitrum" , "avalanche" , "fantom"] as const;
+export type chain = typeof chainState[number];
+
+export const logoState = ["coming soon" , "confirmed" , "looking" , "servers" , "token" , "lazy nft" , "pack" , "bundle" , "chest" , "marketplace"] as const;
+export type logos = typeof logoState[number];
+
 export interface IllustrationProps {
     id?: string;
+
+    /**
+     * set logo
+     */
     logo: chain | logos;
 
     /**
-     * possible sizes are "xs" | "s" | "m" | "l" | "xl"
+     * possible sizes are "xs" , "s" , "m" , "l" , "xl"
      */
     size?: size;
 
@@ -27,7 +40,3 @@ export interface IllustrationProps {
      */
     fillInlineRight?: string
 }
-
-export type size = "xs" | "s" | "m" | "l" | "xl";
-export type chain = "ethereum" | "binance" | "polygon" | "arbitrum" | "avalanche" | "fantom";
-export type logos = "coming soon" | "confirmed" | "looking" | "servers" | "token" | "lazy nft" | "pack" | "bundle" | "chest" | "marketplace";

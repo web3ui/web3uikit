317f4c0d37ea4ba15d778137a843cdfef4a84b8c
dani <martinelli.daniele7@gmail.com>
2022-01-11 14:30:35 +0100

    fix illustrations

--- src/components/Illustrations/types.ts
+++ src/components/Illustrations/types.ts
@@ -1,10 +1,25 @@
-export const sizeState = ["xs", "s", "m", "l", "xl"] as const;
-export type size = typeof sizeState[number];
-
-export const chainState = ["ethereum" , "binance" , "polygon" , "arbitrum" , "avalanche" , "fantom"] as const;
+export const chainState = [
+    'ethereum',
+    'binance',
+    'polygon',
+    'arbitrum',
+    'avalanche',
+    'fantom',
+] as const;
 export type chain = typeof chainState[number];
 
-export const logoState = ["coming soon" , "confirmed" , "looking" , "servers" , "token" , "lazy nft" , "pack" , "bundle" , "chest" , "marketplace"] as const;
+export const logoState = [
+    'coming soon',
+    'confirmed',
+    'looking',
+    'servers',
+    'token',
+    'lazy nft',
+    'pack',
+    'bundle',
+    'chest',
+    'marketplace',
+] as const;
 export type logos = typeof logoState[number];
 
 export interface IllustrationProps {
@@ -14,29 +29,4 @@ export interface IllustrationProps {
      * set logo
      */
     logo: chain | logos;
-
-    /**
-     * possible sizes are "xs" , "s" , "m" , "l" , "xl"
-     */
-    size?: size;
-
-    /**
-     * set inner color
-     */
-    fillInline?: string;
-
-    /**
-     * set border color
-     */
-    fillOutline?: string;
-
-    /**
-     * Use for ethereum and arbitrum only
-     */
-    fillInlineLeft?: string;
-
-    /**
-     * Use for ethereum and arbitrum only
-     */
-    fillInlineRight?: string
 }

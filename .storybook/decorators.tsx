eddd70b8cb6aef3ea3eb369f8767b41e1aa6b558
Y0moo <78314301+Y0moo@users.noreply.github.com>
2022-01-27 15:40:02 +0700

    warn message added

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -26,13 +26,42 @@ export const moralisContext: DecoratorFn = (Story) => {
 
         return null;
     };
+    const MORALIS_APP_ID = process.env.MORALIS_APP_ID;
+    const MORALIS_SERVER_URL = process.env.MORALIS_SERVER_URL;
     return (
-        <MoralisProvider
-            appId={process.env.MORALIS_APP_ID || ''}
-            serverUrl={process.env.MORALIS_SERVER_URL || ''}
-        >
-            <Story />
-            <Web3Initialize />
-        </MoralisProvider>
+        <>
+            {MORALIS_APP_ID && MORALIS_SERVER_URL ? (
+                <MoralisProvider
+                    appId={MORALIS_APP_ID}
+                    serverUrl={MORALIS_SERVER_URL}
+                >
+                    <Story />
+                    <Web3Initialize />
+                </MoralisProvider>
+            ) : (
+                <>
+                    <p>
+                        This component requires your Moralis Server connection!
+                    </p>
+                    <p>
+                        Rename .env.example to .env in the main folder and
+                        provide your appId and serverUrl from Moralis{' '}
+                        <a
+                            href="https://docs.moralis.io/moralis-server/getting-started/create-a-moralis-server"
+                            target="_blank"
+                        >
+                            (How to start Moralis Server)
+                        </a>
+                        <br />
+                        Example: <br />
+                        REACT_APP_MORALIS_APPLICATION_ID = xxxxxxxxxxxx
+                        <br />
+                        REACT_APP_MORALIS_SERVER_URL =
+                        https://xxxxxx.grandmoralis.com:2053/server
+                    </p>
+                    <p>After adding .env run yarn start again</p>
+                </>
+            )}
+        </>
     );
 };

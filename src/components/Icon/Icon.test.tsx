7e3c93e2c252ce3bf6fcb428332e9a73ed42f1a9
oscario2 <oscarandersson550@gmail.com>
2022-02-02 17:41:29 +0100

    test: ensure all icons are mapped

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { IconProps, Icon, iconTypes } from '.';
 import color from '../../styles/colors';
+import { TIconType } from './collection';
 
 export const iconTestId = 'test-icon';
 
@@ -22,7 +23,7 @@ const renderComponent = (props: Partial<IconProps>) => {
     return <Icon {...setState(props)} />;
 };
 
-describe('Icon', () => {
+describe('Icon - render', () => {
     let container: HTMLDivElement;
     const testIcon = 'mail icon';
     const testSize = defaultState.size;
@@ -38,23 +39,46 @@ describe('Icon', () => {
 
     it('renders the component', () => {
         ReactDOM.render(renderComponent({}), container);
-        const icon = container.querySelector(`[data-testid="${iconTestId}"]`);
+        const icon = container.querySelector('svg');
         expect(icon).not.toBeNull();
     });
     it('renders correct SVG', () => {
         ReactDOM.render(renderComponent({ svg: iconTypes.mail }), container);
-        const icon = container.querySelector(`[data-testid="${iconTestId}"]`);
+        const icon = container.querySelector('svg');
         expect(icon?.textContent).toBe(testIcon);
     });
     xit('renders the correct size', () => {
         ReactDOM.render(renderComponent({ size: testSize }), container);
-        const icon = container.querySelector(`[data-testid="${iconTestId}"]`);
+        const icon = container.querySelector('svg');
         expect(icon?.clientHeight).toBe(testSize);
         expect(icon?.clientWidth).toBe(testSize);
     });
     it('renders aria hidden attribute', () => {
         ReactDOM.render(renderComponent({ size: testSize }), container);
-        const icon = container.querySelector(`[data-testid="${iconTestId}"]`);
+        const icon = container.querySelector('svg');
         expect(icon?.getAttribute('aria-hidden')).toBe('true');
+    });
+});
+
+describe('Icon - mapped', () => {
+    let container: HTMLDivElement;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+    });
+    afterEach(() => {
+        document.body.removeChild(container);
+        container.remove();
+    });
+
+    Object.keys(iconTypes).forEach((icon) => {
+        const svg = icon as TIconType;
+
+        it(`should render - ${icon}`, () => {
+            ReactDOM.render(renderComponent({ svg }), container);
+            const found = container.querySelector('svg');
+            expect(found).not.toBeNull();
+        });
     });
 });

8751f02f55300fd7973f4e07953f632cbf524014
Y0moo <78314301+Y0moo@users.noreply.github.com>
2022-02-06 14:54:07 +0700

    feat: added setIsOpened prop for WalletModal

--- src/components/WalletModal/types.ts
+++ src/components/WalletModal/types.ts
@@ -3,4 +3,9 @@ export interface WalletModalProps {
      * Modal is open if true
      */
     isOpened?: boolean;
+
+    /**
+     * Set Open state
+     */
+    setIsOpened: (value: boolean) => void;
 }

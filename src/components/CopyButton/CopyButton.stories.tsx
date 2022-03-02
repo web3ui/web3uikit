51092cfad4ae35e2758aab35741bf58588a78dc2
Y0moo <dmitrii@moralis.io>
2022-03-02 13:10:35 +0400

    feat: story with notififcation added

--- src/components/CopyButton/CopyButton.stories.tsx
+++ src/components/CopyButton/CopyButton.stories.tsx
@@ -1,23 +1,39 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import React from 'react';
 import CopyButton from '../CopyButton/CopyButton';
+import { NotificationProvider, useNotification } from '../Notification';
 import { Typography } from '../Typography';
 
 export default {
     title: '4.UI/CopyButton',
     component: CopyButton,
+    decorators: [
+        (Story) => (
+            <NotificationProvider>
+                <Story />
+            </NotificationProvider>
+        ),
+    ],
 } as ComponentMeta<typeof CopyButton>;
 
-const onTestCopy = (e: any) => {
-    console.log('event: ', e);
+const Template: ComponentStory<typeof CopyButton> = () => {
+    const notify = useNotification();
+    return (
+        <>
+            <Typography>With Love from Developers</Typography>
+            <CopyButton
+                text="With Love from Developers"
+                onCopy={() =>
+                    notify({
+                        type: 'success',
+                        message: 'Copied to clipboard',
+                        title: 'New Notification',
+                        position: 'topR',
+                    })
+                }
+            />
+        </>
+    );
 };
 
-const Template: ComponentStory<typeof CopyButton> = (args) => (
-    <>
-        <Typography>With Love from Developers</Typography>
-        <CopyButton {...args} text="With Love from Developers" />
-    </>
-);
-
 export const CopyButtonDefault = Template.bind({});
-CopyButtonDefault.args = { onCopy: onTestCopy };

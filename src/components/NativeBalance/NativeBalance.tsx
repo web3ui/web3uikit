32b08616a59ce8707d5150396974ed41717194d9
locothedev <locothedev@web.de>
2022-03-13 15:37:10 +0100

    fix(Balance): overflow

--- src/components/NativeBalance/NativeBalance.tsx
+++ src/components/NativeBalance/NativeBalance.tsx
@@ -8,13 +8,14 @@ const { BalanceStyled } = NativeBalanceStyles;
 const NativeBalance: React.FC<NativeBalanceProps> = ({
     style,
 }) => {
-    const { account, chainId, web3 } = useMoralis();
+    const { account, chainId, web3, Moralis } = useMoralis();
     const [balance, setBalance] = useState<{ formatted?: string, balance?: unknown }>({});
 
     useEffect(() => {
         if (account && chainId) {
             web3?.getBalance(account).then((result) => {
-                setBalance({ formatted: String((result.toNumber() / Math.pow(10, 18)).toFixed(8)), balance: result});
+                // eslint-disable-next-line new-cap
+                setBalance({ formatted: String(Number(Moralis.Units.FromWei(result.toString())).toFixed(8)), balance: result});
             });
         }
     }, [account, chainId]);

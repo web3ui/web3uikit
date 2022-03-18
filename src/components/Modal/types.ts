42a07a926bbcb054798bfd146bda8dd845565c29
dani <martinelli.daniele7@gmail.com>
2022-03-18 03:59:00 +0100

    feat: docs

--- src/components/Modal/types.ts
+++ src/components/Modal/types.ts
@@ -85,7 +85,7 @@ export interface ModalProps {
     customFooter?: JSX.Element;
 
     /**
-     * To have a custom close button
+     * To have a custom close button. The button id must be set to `close`.
      */
     closeButton?: ButtonProps;
 }

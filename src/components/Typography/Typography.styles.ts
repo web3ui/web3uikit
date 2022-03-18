20aabebb3aca356b708c8e1d424aa9765ef5568b
rayyan224 <rayyan5926439@gmail.com>
2022-03-18 15:43:18 -0400

    feat: changed typography url to support local

--- src/components/Typography/Typography.styles.ts
+++ src/components/Typography/Typography.styles.ts
@@ -3,70 +3,79 @@ import color from '../../styles/colors';
 import type { TypographyProps, variantType, weightType } from './types';
 
 const openSans = css`
-    @import url('//fonts.googleapis.com/css?family=Open+Sans');
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans');
     font-family: 'Open Sans', sans-serif;
 `;
 const argentumSans = css`
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/76f34d19f76c1aeaf5b5b59d9337b83f/ArgentumSansBlack.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/76f34d19f76c1aeaf5b5b59d9337b83f/ArgentumSansBlack.ttf')
+                format('opentype');
         font-weight: 800;
         font-style: normal;
     }
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/d57a3801b79be79d871a9d09f3ee3c12/ArgentumSansBold.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/d57a3801b79be79d871a9d09f3ee3c12/ArgentumSansBold.ttf')
+                format('opentype');
         font-weight: 700;
         font-style: normal;
     }
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/7f1fb27c262e3a9af6389bb4ab5b5f53/ArgentumSansLight.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/7f1fb27c262e3a9af6389bb4ab5b5f53/ArgentumSansLight.ttf')
+                format('opentype');
         font-weight: 200;
         font-style: normal;
     }
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/dfdbaee7cbe33f00a439f4cc1fb02c70/ArgentumSansLightItalic.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/dfdbaee7cbe33f00a439f4cc1fb02c70/ArgentumSansLightItalic.ttf')
+                format('opentype');
         font-weight: 200;
         font-style: italic;
     }
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/e37732e4248ec8c6c333010a98620f0b/ArgentumSansMedium.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/e37732e4248ec8c6c333010a98620f0b/ArgentumSansMedium.ttf')
+                format('opentype');
         font-weight: 500;
         font-style: normal;
     }
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/c71775e3bd01672203acd6a7747652e6/ArgentumSansMediumItalic.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/c71775e3bd01672203acd6a7747652e6/ArgentumSansMediumItalic.ttf')
+                format('opentype');
         font-weight: 500;
         font-style: italic;
     }
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/065ae922c6d96030bc9d4ba7c0e5f9dc/ArgentumSansRegular.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/065ae922c6d96030bc9d4ba7c0e5f9dc/ArgentumSansRegular.ttf')
+                format('opentype');
         font-weight: 400;
         font-style: normal;
     }
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/3ef4fc069019bcba041e2bf33dd650c8/ArgentumSansSemiBold.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/3ef4fc069019bcba041e2bf33dd650c8/ArgentumSansSemiBold.ttf')
+                format('opentype');
         font-weight: 600;
         font-style: normal;
     }
     @font-face {
         font-family: 'Argentum Sans';
-        src: url('//fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/4b9a3149d9c2fa53aeb22d9482e758e1/ArgentumSansSemiBoldItalic.ttf')
-            format('opentype');
+        src: local('Argentum Sans'),
+            url('https://fontlibrary.org/assets/fonts/argentum-sans/9d2a529d403d28ea06c79fc969fe3cbf/4b9a3149d9c2fa53aeb22d9482e758e1/ArgentumSansSemiBoldItalic.ttf')
+                format('opentype');
         font-weight: 600;
         font-style: italic;
     }

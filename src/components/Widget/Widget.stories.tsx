91aa5c2901bf73a951d6efec20149b4b9c9ebab5
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-04-07 09:15:39 +0100

    widget refactor

--- src/components/Widget/Widget.stories.tsx
+++ src/components/Widget/Widget.stories.tsx
@@ -11,19 +11,19 @@ export default {
 const Template: ComponentStory<typeof Widget> = (args) => <Widget {...args} />;
 
 const Row: ComponentStory<typeof Widget> = () => (
-    <div style={{ display: 'grid', gap: '20px', padding: '40px' }}>
-        <div style={{ display: 'flex', gap: '20px' }}>
+    <div style={{ display: 'grid', gap: '20px', padding: '40px 20px' }}>
+        <section style={{ display: 'flex', gap: '20px' }}>
             <Widget info="Mainnet" title="ENVIRONMENT" />
             <Widget info="233,182" title="NUMBER OF USERS">
                 <div>CHART COMING SOON</div>
             </Widget>
-        </div>
-        <div style={{ display: 'flex', gap: '20px' }}>
+        </section>
+        <section style={{ display: 'flex', gap: '20px' }}>
             <Widget info="72%" title="CPU" />
             <Widget info="0 Bytes" title="NETWORK" />
             <Widget info="24%" title="RAM" />
             <Widget info="24%" title="DISK" />
-        </div>
+        </section>
     </div>
 );
 

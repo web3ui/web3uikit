ee7be5a7f75e510b278b3da8b528e077f84e2e90
Abhinav MV <mvabhinav1998@gmail.com>
2022-04-07 22:28:32 +0530

    React18 refactor banner strip and styles

--- src/components/BannerStrip/BannerStrip.tsx
+++ src/components/BannerStrip/BannerStrip.tsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import bannerStripStyles from './BannerStrip.styles';
-import color from '../../styles/colors';
-import fonts from '../../styles/fonts';
 import { BannerStripProps } from '.';
 import { Button } from '../Button';
+import styles from './BannerStrip.styles';
 
-const getBackgroundColor = (type: string) => {
-    switch (type) {
-        case 'success':
-            return color.green;
-        case 'warning':
-            return color.yellow;
-        case 'error':
-            return color.red;
-        default:
-            return color.blue;
-    }
-};
-
-const Section = styled.section<Pick<BannerStripProps, 'type' | 'height'>>`
-    ${fonts.text};
-    ${bannerStripStyles.section};
-    background-color: ${(p) => p.type && getBackgroundColor(p.type)};
-    height: ${({ height }) => height};
-`;
+const { SectionStyled } = styles;
 
 const BannerStrip: React.FC<BannerStripProps> = ({
     buttonConfig,
@@ -33,10 +12,10 @@ const BannerStrip: React.FC<BannerStripProps> = ({
     text,
     type = 'standard',
 }) => (
-    <Section type={type} height={height} data-testid="banner-strip">
+    <SectionStyled type={type} height={height} data-testid="banner-strip">
         <strong>{text}</strong>
         {buttonDisplayed && <Button {...buttonConfig} />}
-    </Section>
+    </SectionStyled>
 );
 
 export default BannerStrip;

6c73fa613f8245d1566fbc8cd93795b0588b9ede
Abhinav MV <mvabhinav1998@gmail.com>
2022-04-11 23:47:35 +0530

    React18 refactor cryto logos

--- src/components/CryptoLogos/CryptoLogos.test.tsx
+++ src/components/CryptoLogos/CryptoLogos.test.tsx
@@ -1,4 +1,3 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './CryptoLogos.stories';
@@ -6,197 +5,63 @@ import rgbToHex from '../../utils/rgbToHex';
 import color from '../../styles/colors';
 import 'jest-styled-components';
 import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
 
 const { Ethereum, Binance, Polygon, Avalanche, Fantom, Arbitrum } =
     composeStories(stories);
 
-let container: HTMLDivElement;
+const cryptoLogoId = 'test-crypto-logo';
 
-describe('Ethereum', () => {
-    const CryptoLogoId = 'test-crypto-logo';
-
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<Ethereum bgColor={color.black} />, container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
-
-    test('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
-
-    test('checks the background color', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        const styles = element && getComputedStyle(element);
-        const bgColorHex =
-            styles && rgbToHex(styles.backgroundColor).toUpperCase();
-        expect(bgColorHex).toBe(color.black);
-    });
+test('Renders - Ethereum Logo', () => {
+    render(<Ethereum bgColor={color.black} />);
+    const logoElement = screen.getByTestId(cryptoLogoId);
+    expect(logoElement).not.toBeNull();
+    const styles = logoElement && getComputedStyle(logoElement);
+    const bgColorHex = styles && rgbToHex(styles.backgroundColor).toUpperCase();
+    expect(bgColorHex).toBe(color.black);
 });
 
-describe('Binance', () => {
-    const CryptoLogoId = 'test-crypto-logo';
-
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<Binance bgColor={color.black} />, container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
-
-    test('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
-
-    test('checks the background color', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        const styles = element && getComputedStyle(element);
-        const bgColorHex =
-            styles && rgbToHex(styles.backgroundColor).toUpperCase();
-        expect(bgColorHex).toBe(color.black);
-    });
+test('Renders - Binance Logo', () => {
+    render(<Binance bgColor={color.black} />);
+    const logoElement = screen.getByTestId(cryptoLogoId);
+    expect(logoElement).not.toBeNull();
+    const styles = logoElement && getComputedStyle(logoElement);
+    const bgColorHex = styles && rgbToHex(styles.backgroundColor).toUpperCase();
+    expect(bgColorHex).toBe(color.black);
 });
 
-describe('Polygon', () => {
-    const CryptoLogoId = 'test-crypto-logo';
-
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<Polygon bgColor={color.black} />, container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
-
-    test('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
-
-    test('checks the background color', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        const styles = element && getComputedStyle(element);
-        const bgColorHex =
-            styles && rgbToHex(styles.backgroundColor).toUpperCase();
-        expect(bgColorHex).toBe(color.black);
-    });
+test('Renders - Polygon Logo', () => {
+    render(<Polygon bgColor={color.black} />);
+    const logoElement = screen.getByTestId(cryptoLogoId);
+    expect(logoElement).not.toBeNull();
+    const styles = logoElement && getComputedStyle(logoElement);
+    const bgColorHex = styles && rgbToHex(styles.backgroundColor).toUpperCase();
+    expect(bgColorHex).toBe(color.black);
 });
 
-describe('Avalanche', () => {
-    const CryptoLogoId = 'test-crypto-logo';
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<Avalanche bgColor={color.black} />, container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
-
-    test('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
-
-    test('checks the background color', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        const styles = element && getComputedStyle(element);
-        const bgColorHex =
-            styles && rgbToHex(styles.backgroundColor).toUpperCase();
-        expect(bgColorHex).toBe(color.black);
-    });
+test('Renders - Avalanche Logo', () => {
+    render(<Avalanche bgColor={color.black} />);
+    const logoElement = screen.getByTestId(cryptoLogoId);
+    expect(logoElement).not.toBeNull();
+    const styles = logoElement && getComputedStyle(logoElement);
+    const bgColorHex = styles && rgbToHex(styles.backgroundColor).toUpperCase();
+    expect(bgColorHex).toBe(color.black);
 });
 
-describe('Fantom', () => {
-    const CryptoLogoId = 'test-crypto-logo';
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<Fantom bgColor={color.black} />, container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
-
-    test('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
-
-    test('checks the background color', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        const styles = element && getComputedStyle(element);
-        const bgColorHex =
-            styles && rgbToHex(styles.backgroundColor).toUpperCase();
-        expect(bgColorHex).toBe(color.black);
-    });
+test('Renders - Fantom Logo', () => {
+    render(<Fantom bgColor={color.black} />);
+    const logoElement = screen.getByTestId(cryptoLogoId);
+    expect(logoElement).not.toBeNull();
+    const styles = logoElement && getComputedStyle(logoElement);
+    const bgColorHex = styles && rgbToHex(styles.backgroundColor).toUpperCase();
+    expect(bgColorHex).toBe(color.black);
 });
 
-describe('Arbitrum', () => {
-    const CryptoLogoId = 'test-crypto-logo';
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<Arbitrum bgColor={color.black} />, container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
-
-    test('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
-
-    test('checks the background color', () => {
-        const element = container.querySelector(
-            `[data-testid="${CryptoLogoId}"]`,
-        );
-        const styles = element && getComputedStyle(element);
-        const bgColorHex =
-            styles && rgbToHex(styles.backgroundColor).toUpperCase();
-        expect(bgColorHex).toBe(color.black);
-    });
+test('Renders - Arbitrum Logo', () => {
+    render(<Arbitrum bgColor={color.black} />);
+    const logoElement = screen.getByTestId(cryptoLogoId);
+    expect(logoElement).not.toBeNull();
+    const styles = logoElement && getComputedStyle(logoElement);
+    const bgColorHex = styles && rgbToHex(styles.backgroundColor).toUpperCase();
+    expect(bgColorHex).toBe(color.black);
 });

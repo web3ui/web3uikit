80b3d009833e39c9d1a85da3ee583107d2e382cf
Nikhil Ranjan <niklabh811@gmail.com>
2022-04-12 22:25:29 +0200

    feat: React18 refactor Logo

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -1,117 +1,71 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './Logo.stories';
+import {
+    render,
+    screen,
+} from '@testing-library/react';
 
 const {
-    LogoIconWhite,
-    LogoDefaultWhite,
-    VisaRegular,
-    MastercardRegular,
     AmexRegular,
     DinersRegular,
+    LogoDefaultWhite,
+    LogoIconWhite,
+    MastercardRegular,
+    VisaRegular,
 } = composeStories(stories);
 
-let container: HTMLDivElement;
 
-describe('Logo - Icon', () => {
+test('Renders Logo Icon', () => {
     const testlogoIconId = 'test-logo-icon';
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<LogoIconWhite />, container);
-    });
+    render(<LogoIconWhite />);
 
-    it('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${testlogoIconId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
+    const logoIcon = screen.getByTestId(testlogoIconId);
+    expect(logoIcon).not.toBeNull();
 });
 
-describe('Logo - Default', () => {
+test('Renders Logo Default', () => {
     const testlogoIconId = 'test-logo-default';
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<LogoDefaultWhite />, container);
-    });
+    render(<LogoDefaultWhite />);
 
-    it('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${testlogoIconId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
+    const logoIcon = screen.getByTestId(testlogoIconId);
+    expect(logoIcon).not.toBeNull();
 });
 
-describe('Logo - Visa', () => {
+test('Renders Logo Visa', () => {
     const testlogoIconId = 'test-logo-visa';
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<VisaRegular />, container);
-    });
+    render(<VisaRegular />);
 
-    it('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${testlogoIconId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
+    const logoIcon = screen.getByTestId(testlogoIconId);
+    expect(logoIcon).not.toBeNull();
 });
 
-describe('Logo - Mastercard', () => {
+test('Renders Logo Mastercard', () => {
     const testlogoIconId = 'test-logo-mastercard';
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<MastercardRegular />, container);
-    });
+    render(<MastercardRegular />);
 
-    it('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${testlogoIconId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
+    const logoIcon = screen.getByTestId(testlogoIconId);
+    expect(logoIcon).not.toBeNull();
 });
 
-describe('Logo - American Express', () => {
+test('Renders Logo American Express', () => {
     const testlogoIconId = 'test-logo-amex';
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<AmexRegular />, container);
-    });
+    render(<AmexRegular />);
 
-    it('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${testlogoIconId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
+    const logoIcon = screen.getByTestId(testlogoIconId);
+    expect(logoIcon).not.toBeNull();
 });
 
-describe('Logo - Diners Club', () => {
+test('Renders Logo Diners Club', () => {
     const testlogoIconId = 'test-logo-diners';
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<DinersRegular />, container);
-    });
+    render(<DinersRegular />);
 
-    it('renders the component', () => {
-        const element = container.querySelector(
-            `[data-testid="${testlogoIconId}"]`,
-        );
-        expect(element).not.toBeNull();
-    });
+    const logoIcon = screen.getByTestId(testlogoIconId);
+    expect(logoIcon).not.toBeNull();
 });

e152e5bdb33cc97a471deb048329659e6a033361
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-04-13 10:34:45 +0100

    copleted notification test

--- src/components/Notification/Notification.test.tsx
+++ src/components/Notification/Notification.test.tsx
@@ -1,166 +1,67 @@
 import { composeStories } from '@storybook/testing-react';
-import 'jest-styled-components';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import * as stories from './Notification.stories';
+
 const { Regular, Standard, CustomIcon } = composeStories(stories);
 
-describe('Notification - Standard - Active - Regular Text - Regular Icon', () => {
-    let container: HTMLDivElement;
-    const message = Standard.args?.message;
-    const messageId = 'test-notification-message';
-    const testId = 'test-notification-id';
-    const closeId = 'test-notification-x';
-    const title = 'New Message';
-    const iconId = 'test-notification-icon-wrapper';
-    const titleId = 'test-notification-title';
+const messageId = 'test-notification-message';
+const testId = 'test-notification-id';
+const closeId = 'test-notification-x';
+const titleMessage = 'New Message';
+const iconId = 'test-notification-icon-wrapper';
+const titleId = 'test-notification-title';
 
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<Standard />, container);
-    });
+type TestStoryProps = {
+    name: string;
+    Component: any;
+};
 
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
+function testStory({ name, Component }: TestStoryProps) {
+    return test(name, () => {
+        const title = Component?.args?.title || titleMessage;
+        const message = Component?.args?.message;
 
-    it('renders the component', () => {
-        const element = container.querySelector(`[data-testid="${testId}"]`);
+        render(<Component />);
+
+        // renders the component
+        const element = screen.getByTestId(testId);
         expect(element).not.toBeNull();
+
+        // renders title correctly
+        const titleElement = screen.getByTestId(titleId);
+        expect(titleElement?.textContent).toBe(title);
+
+        // renders message correctly
+        const messageElement = screen.getByTestId(messageId);
+        expect(messageElement?.textContent).toBe(message);
+
+        // should render left icon
+        const iconWrapper = screen.getByTestId(iconId);
+        expect(iconWrapper).not.toBeNull();
+
+        // should render close icon
+        const closeIconWrapper = screen.getByTestId(closeId);
+        expect(closeIconWrapper).not.toBeNull();
     });
+}
 
-    it('renders title correctly', () => {
-        const element = container.querySelector(`[data-testid="${titleId}"]`);
-        expect(element?.textContent).toBe(title);
-    });
+const data = [
+    {
+        name: 'Notification - Standard - Active - Regular Text - Regular Icon',
+        Component: Standard,
+    },
+    {
+        name: 'Notification - Regular - Active - Custom Text - Regular Icon',
+        Component: Regular,
+    },
+    {
+        name: 'Notification - Active - Custom Text - Custom Icon',
+        Component: CustomIcon,
+    },
+];
 
-    it('renders message correctly', () => {
-        const element = container.querySelector(`[data-testid="${messageId}"]`);
-        expect(element?.textContent).toBe(message);
-    });
-
-    it('should render left icon', () => {
-        const iconSVG = container.querySelector(
-            `[data-testid="${iconId}"] > div > svg`,
-        );
-        expect(iconSVG).not.toBeNull();
-    });
-
-    it('should render close icon', () => {
-        const closeSVGtitle = container.querySelector(
-            `[data-testid="${closeId}"] > div > svg > title`,
-        );
-        expect(closeSVGtitle?.innerHTML).toBe('x icon');
-    });
-
-    it('should render correct', () => {
-        const iconSVG = container.querySelector(
-            `[data-testid="${closeId}"] > div > svg`,
-        );
-        expect(iconSVG).not.toBeNull();
-    });
-});
-
-describe('Notification - Regular - Active - Custom Text - Regular Icon', () => {
-    let container: HTMLDivElement;
-    const testId = 'test-notification-id';
-    const messageId = 'test-notification-message';
-    const titleId = 'test-notification-title';
-    const closeId = 'test-notification-x';
-    const title = Regular?.args?.title;
-    const message = Regular?.args?.message;
-    const iconId = 'test-notification-icon-wrapper';
-
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<Regular />, container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
-
-    it('renders the component', () => {
-        const element = container.querySelector(`[data-testid="${testId}"]`);
-        expect(element).not.toBeNull();
-    });
-
-    it('renders title correctly', () => {
-        const element = container.querySelector(`[data-testid="${titleId}"]`);
-        expect(element?.textContent).toBe(title);
-    });
-
-    it('renders message correctly', () => {
-        const element = container.querySelector(`[data-testid="${messageId}"]`);
-        expect(element?.textContent).toBe(message);
-    });
-
-    it('should render left icon', () => {
-        const iconSVG = container.querySelector(
-            `[data-testid="${iconId}"] > div > svg`,
-        );
-        expect(iconSVG).not.toBeNull();
-    });
-
-    it('should render close icon', () => {
-        const iconSVG = container.querySelector(
-            `[data-testid="${closeId}"] > div > svg`,
-        );
-        expect(iconSVG).not.toBeNull();
-    });
-});
-
-describe('Notification - Active - Custom Text - Custom Icon', () => {
-    let container: HTMLDivElement;
-    const testId = 'test-notification-id';
-    const messageId = 'test-notification-message';
-    const titleId = 'test-notification-title';
-    const closeId = 'test-notification-x';
-    const iconId = 'test-notification-icon-wrapper';
-    const title = CustomIcon?.args?.title;
-    const message = CustomIcon?.args?.message;
-
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-        ReactDOM.render(<CustomIcon />, container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
-        container.remove();
-    });
-
-    it('renders the component', () => {
-        const element = container.querySelector(`[data-testid="${testId}"]`);
-        expect(element).not.toBeNull();
-    });
-
-    it('renders title correctly', () => {
-        const element = container.querySelector(`[data-testid="${titleId}"]`);
-        expect(element?.textContent).toBe(title);
-    });
-
-    it('renders message correctly', () => {
-        const element = container.querySelector(`[data-testid="${messageId}"]`);
-        expect(element?.textContent).toBe(message);
-    });
-
-    it('should render left icon', () => {
-        const iconSVG = container.querySelector(
-            `[data-testid="${iconId}"] > div > svg`,
-        );
-        expect(iconSVG).not.toBeNull();
-    });
-
-    it('should render close icon', () => {
-        const iconSVG = container.querySelector(
-            `[data-testid="${closeId}"] > div > svg`,
-        );
-        expect(iconSVG).not.toBeNull();
-    });
-});
+for (let index = 0; index < data.length; index++) {
+    const element = data[index];
+    testStory({ ...element });
+}

d29b48a1461a282e819a5211883e0dd8c61d93a3
Darrel Idiagbor <idiagbordarrel@gmail.com>
2022-05-30 18:49:16 +0100

    chore: move font breakpoint into fonts rather than standalone files

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,74 @@
 import color from './colors';
-import fontBreakpoint, { fontSize } from './fontBreakpoint';
+import breakpoints from './breakpoints';
 import { css } from 'styled-components';
+
+export const fontSize = {
+    h1: 36,
+    h2: 28,
+    h3: 24,
+    h4: 20,
+    h5: 16,
+};
+
+const fontBreakpoint = {
+    h1: css`
+        @media screen and (max-width: ${breakpoints.xl}) {
+            font-size: ${fontSize.h1 - 2}px;
+        }
+
+        @media screen and (max-width: ${breakpoints.lg}) {
+            font-size: ${fontSize.h1 - 4}px;
+        }
+
+        @media screen and (max-width: ${breakpoints.md}) {
+            font-size: ${fontSize.h1 - 6}px;
+        }
+
+        @media screen and (max-width: ${breakpoints.sm}) {
+            font-size: ${fontSize.h1 - 8}px;
+        }
+    `,
+    h2: css`
+        @media screen and (max-width: ${breakpoints.xl}) {
+            font-size: ${fontSize.h2 - 2}px;
+        }
+
+        @media screen and (max-width: ${breakpoints.lg}) {
+            font-size: ${fontSize.h2 - 4}px;
+        }
+
+        @media screen and (max-width: ${breakpoints.md}) {
+            font-size: ${fontSize.h2 - 6}px;
+        }
+    `,
+    h3: css`
+        @media screen and (max-width: ${breakpoints.xl}) {
+            font-size: ${fontSize.h3 - 2}px;
+        }
+
+        @media screen and (max-width: ${breakpoints.lg}) {
+            font-size: ${fontSize.h3 - 4}px;
+        }
+
+        @media screen and (max-width: ${breakpoints.md}) {
+            font-size: ${fontSize.h3 - 6}px;
+        }
+    `,
+    h4: css`
+        @media screen and (max-width: ${breakpoints.lg}) {
+            font-size: ${fontSize.h4 - 2}px;
+        }
+
+        @media screen and (max-width: ${breakpoints.md}) {
+            font-size: ${fontSize.h4 - 4}px;
+        }
+    `,
+    h5: css`
+        @media screen and (max-width: ${breakpoints.md}) {
+            font-size: ${fontSize.h5 - 2}px;
+        }
+    `,
+};
 
 const heading = css`
     -webkit-font-smoothing: antialiased;

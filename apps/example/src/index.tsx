7f3f2602816196b743d3da43d5b610d95e4456e4
Osama Jandali <osamajandaly@gmail.com>
2022-06-03 13:28:34 +0200

    Add NFT to ui package

--- apps/example/src/index.tsx
+++ apps/example/src/index.tsx
@@ -1,29 +1,46 @@
-import { Route, Routes } from 'react-router-dom';
-import { lazily } from 'react-lazily';
-import { Suspense } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { lazily } from "react-lazily";
+import { Suspense } from "react";
 
-const { Button, Logo, Typography, Avatar, BannerStrip, VerifyCode } = lazily(() => import('@web3uikit/core'));
-// const {  } = lazily(() => import('@web3uikit/ui'));
+const { Button, Logo, Typography, Avatar, BannerStrip, VerifyCode } = lazily(
+  () => import("@web3uikit/core")
+);
+const { ConnectWallet, NFT } = lazily(() => import("@web3uikit/ui"));
 export const App = () => {
   return (
     <Routes>
       <Route
-        path='/'
+        path="/"
         element={
           <Suspense fallback={<>Loading...</>}>
-            <div style={{ display: 'flex', flexDirection: 'column', margin: '20px' }}>
-              <Logo theme='default' color='white' />
-              <Button size='large' text='Large' isFullWidth theme='primary' />
-              <Typography children='text' variant='h1' />
-              <Avatar theme='image' />
-              <BannerStrip text='Hey this is a notification you should check out' type='standard' />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "20px",
+              }}
+            >
+              <Logo theme="default" color="white" />
+              <Button size="large" text="Large" isFullWidth theme="primary" />
+              <Typography children="text" variant="h1" />
+              <Avatar theme="image" />
+              <BannerStrip
+                text="Hey this is a notification you should check out"
+                type="standard"
+              />
               <VerifyCode />
             </div>
-            {/* <ConnectWallet /> */}
+            <ConnectWallet />
+            <NFT
+              address="0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"
+              chain="eth"
+              fetchMetadata
+              tokenId="1"
+            />
           </Suspense>
         }
       />
-      <Route path='*' element={<>Not found...</>}></Route>
+      <Route path="*" element={<>Not found...</>}></Route>
     </Routes>
   );
 };

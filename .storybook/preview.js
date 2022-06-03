1331ce633d5502420fbdb5dc5dca553bd9329d31
Abhinav MV <mvabhinav1998@gmail.com>
2022-06-03 20:02:24 +0530

    chore: sort stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,12 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
+      date: /Date$/
+    }
   },
-}
+  options: {
+    storySort: (a, b) => (a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }))
+  }
+};

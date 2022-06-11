494ae435fa1ccc531d25db3a50e67e6f4fbd5935
oscario2 <oscarandersson550@gmail.com>
2022-06-11 05:21:02 -0700

    chore(ui): add peer dependencies

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig, UserConfigExport } from 'vite';
 import config from './node_modules/@web3uikit/config/vite.config';
 
 export default defineConfig({
-    ...(config(__dirname, ['react', 'react-dom']) as UserConfigExport),
+    ...(config(__dirname, [
+        'react',
+        'react-dom',
+        'moralis',
+        'react-moralis',
+    ]) as UserConfigExport),
 });

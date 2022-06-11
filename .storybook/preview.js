312e678716d935542a3e990df0ab9909593b7aae
oscario2 <oscarandersson550@gmail.com>
2022-06-11 05:10:55 -0700

    feat(storybook): vite + chore(pnpm): hoist storybook
    `storiesOf` are deprecated with `storyStoreV7` so `Icon.stories.tsx` is being explicitly ignored in storybook until it's been refactored

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,15 @@
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/
-    }
-  },
-  options: {
-    storySort: (a, b) => (a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }))
-  }
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+    options: {
+        storySort: (a, b) =>
+            a.title === b.title
+                ? 0
+                : a.id.localeCompare(b.id, undefined, { numeric: true }),
+    },
 };
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfigExport } from 'vite';
 import config from './node_modules/@web3uikit/config/vite.config';
 
 export default defineConfig({
-    ...config(__dirname, ['react', 'react-dom', 'moralis', 'react-moralis']),
+    ...(config(__dirname, ['react', 'react-dom']) as UserConfigExport),
 });

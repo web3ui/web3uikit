a287b9f35c653512777668418761ad1a3031f956
Abhinav MV <mvabhinav1998@gmail.com>
2022-06-19 17:40:45 +0530

    chore: cronos story in cryto logos

--- packages/core/src/lib/CryptoLogos/CryptoLogos.stories.tsx
+++ packages/core/src/lib/CryptoLogos/CryptoLogos.stories.tsx
@@ -3,45 +3,53 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import CryptoLogos from './CryptoLogos';
 
 export default {
-  title: '6.Graphic/Crypto Logos',
-  component: CryptoLogos,
-  argTypes: { onClick: { action: 'clicked' } }
+    title: '6.Graphic/Crypto Logos',
+    component: CryptoLogos,
+    argTypes: { onClick: { action: 'clicked' } },
 } as ComponentMeta<typeof CryptoLogos>;
 
-const Template: ComponentStory<typeof CryptoLogos> = (args) => <CryptoLogos {...args} />;
+const Template: ComponentStory<typeof CryptoLogos> = (args) => (
+    <CryptoLogos {...args} />
+);
 
 export const Ethereum = Template.bind({});
 Ethereum.args = {
-  chain: 'ethereum',
-  size: '48px'
+    chain: 'ethereum',
+    size: '48px',
 };
 
 export const Binance = Template.bind({});
 Binance.args = {
-  chain: 'binance',
-  size: '48px'
+    chain: 'binance',
+    size: '48px',
 };
 
 export const Polygon = Template.bind({});
 Polygon.args = {
-  chain: 'polygon',
-  size: '48px'
+    chain: 'polygon',
+    size: '48px',
 };
 
 export const Avalanche = Template.bind({});
 Avalanche.args = {
-  chain: 'avalanche',
-  size: '48px'
+    chain: 'avalanche',
+    size: '48px',
 };
 
 export const Fantom = Template.bind({});
 Fantom.args = {
-  chain: 'fantom',
-  size: '48px'
+    chain: 'fantom',
+    size: '48px',
 };
 
 export const Arbitrum = Template.bind({});
 Arbitrum.args = {
-  chain: 'arbitrum',
-  size: '48px'
+    chain: 'arbitrum',
+    size: '48px',
+};
+
+export const Cronos = Template.bind({});
+Cronos.args = {
+    chain: 'cronos',
+    size: '48px',
 };

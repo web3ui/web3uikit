7c82816748d49480814f62c36a046c574e2e63bb
Hritwik <triptathi.hritwik@gmail.com>
2022-07-03 01:39:34 +0530

    changes done

--- src/components/ENSAvatar/ENSAvatar.tsx
+++ src/components/ENSAvatar/ENSAvatar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ENSAvatarprops } from './types';
-import { useMoralis } from 'react-moralis';
+import { useMoralis, useEnsAddress, useEnsName } from 'react-moralis';
 import Blockies from 'react-blockies';
 import { ENSAvatarStyles } from './ENSAvatar.styles';
 
@@ -8,35 +8,31 @@ const { NonAvatarDiv, AvatarImg } = ENSAvatarStyles;
 
 const ENSAvatar: React.FC<ENSAvatarprops> = (props) => {
     const { account, chainId, web3 } = useMoralis();
-    const [ensName, setensName] = useState<string | null>(null);
-    const [AvatarURI, setAvatarURI] = useState<string | undefined>();
+    const { name, isLoading, error } = useEnsAddress(String(account));
+
+    console.log(account);
+    // const [ensName, setensName] = useState<string | null>(null);
+    const [AvatarURI, setAvatarURI] = useState<string | null>(null);
 
     useEffect(() => {
-        if (account && chainId) {
-            web3?.lookupAddress(account).then((result) => {
-                setensName(result);
-            });
-
-            web3?.getAvatar(String(ensName)).then((r) => {
+        if (account && chainId && name) {
+            web3?.getAvatar(account).then((r) => {
                 setAvatarURI(String(r));
+                console.log(r);
             });
         }
-    }, [account, chainId]);
+    }, [account, chainId, name]);
 
     return (
         <>
-            {AvatarURI == String(null) ? (
-                <>
-                    <NonAvatarDiv>
-                        <Blockies seed={props.seed} />
-                    </NonAvatarDiv>
-                </>
+            {AvatarURI ? (
+                <div>
+                    <AvatarImg src={AvatarURI} />
+                </div>
             ) : (
-                <>
-                    <div>
-                        <AvatarImg src={AvatarURI} />
-                    </div>
-                </>
+                <NonAvatarDiv>
+                    <Blockies seed={props.seed} />
+                </NonAvatarDiv>
             )}
         </>
     );

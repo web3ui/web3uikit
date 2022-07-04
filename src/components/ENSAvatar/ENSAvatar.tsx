179cf4c225314fe45de55ff867864ba1590bad76
Hritwik <triptathi.hritwik@gmail.com>
2022-07-05 00:28:33 +0530

    changes done

--- src/components/ENSAvatar/ENSAvatar.tsx
+++ src/components/ENSAvatar/ENSAvatar.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ENSAvatarprops } from './types';
-import { useMoralis, useEnsAddress, useEnsName } from 'react-moralis';
+import { useMoralis } from 'react-moralis';
 import Blockies from 'react-blockies';
 import { ENSAvatarStyles } from './ENSAvatar.styles';
 
@@ -8,18 +8,17 @@ const { NonAvatarDiv, AvatarImg } = ENSAvatarStyles;
 
 const ENSAvatar: React.FC<ENSAvatarprops> = (props) => {
     const { account, chainId, web3 } = useMoralis();
-    const { name, isLoading, error } = useEnsAddress(String(account));
+    
 
-    // const [ensName, setensName] = useState<string | null>(null);
     const [AvatarURI, setAvatarURI] = useState<string | null>(null);
 
     useEffect(() => {
-        if (account && chainId && name) {
+        if (account && chainId) {
             web3?.getAvatar(account).then((r) => {
                 setAvatarURI(String(r));
             });
         }
-    }, [account, chainId, name]);
+    }, [account, chainId]);
 
     return (
         <>

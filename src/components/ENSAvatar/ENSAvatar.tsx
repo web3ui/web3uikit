daa011769421db5d928af02fe6e6f89c09854317
Hritwik Tripathi <52003051+3scava1i3r@users.noreply.github.com>
2022-07-04 14:39:27 +0530

    removing console.logs

--- src/components/ENSAvatar/ENSAvatar.tsx
+++ src/components/ENSAvatar/ENSAvatar.tsx
@@ -10,7 +10,6 @@ const ENSAvatar: React.FC<ENSAvatarprops> = (props) => {
     const { account, chainId, web3 } = useMoralis();
     const { name, isLoading, error } = useEnsAddress(String(account));
 
-    console.log(account);
     // const [ensName, setensName] = useState<string | null>(null);
     const [AvatarURI, setAvatarURI] = useState<string | null>(null);
 
@@ -18,7 +17,6 @@ const ENSAvatar: React.FC<ENSAvatarprops> = (props) => {
         if (account && chainId && name) {
             web3?.getAvatar(account).then((r) => {
                 setAvatarURI(String(r));
-                console.log(r);
             });
         }
     }, [account, chainId, name]);

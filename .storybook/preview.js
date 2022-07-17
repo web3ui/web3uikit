5ec1a0dbfccca713265924450d870f455a1f157c
BillyG83 <bgilmore83@hotmail.co.uk>
2022-07-17 21:57:28 +0200

    fix(Storybook): background colors

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,6 +4,55 @@ initialize({ onUnhandledRequest: 'bypass' });
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    backgrounds: {
+        default: 'twitter',
+        values: [
+            {
+                name: 'white',
+                value: '#ffffff',
+            },
+            {
+                name: 'light',
+                value: '#f9f9f9',
+            },
+            {
+                name: 'grey',
+                value: '#ccc',
+            },
+            {
+                name: 'dark',
+                value: '#444',
+            },
+            {
+                name: 'black',
+                value: '#444',
+            },
+            {
+                name: 'green',
+                value: '#21BF96',
+            },
+            {
+                name: 'yellow',
+                value: '#ECA609',
+            },
+            {
+                name: 'red',
+                value: '#EB5757',
+            },
+            {
+                name: 'blue',
+                value: '#2E7DAF',
+            },
+            {
+                name: 'purple',
+                value: '#8851DA',
+            },
+            {
+                name: 'pink',
+                value: '#DA51BE',
+            },
+        ],
+    },
     controls: {
         matchers: {
             color: /(background|color)$/i,

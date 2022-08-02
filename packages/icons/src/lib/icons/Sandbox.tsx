0a291e25c0535778328042989c07072ee0d8709a
AbhinavMV <mvabhinav1998@gmail.com>
2022-08-02 21:22:12 +0530

    feat(Icons): Add new icons

---
+++ packages/icons/src/lib/icons/Sandbox.tsx
@@ -0,0 +1,30 @@
+// Auto-generated file created by svgr-cli source svg-template.js
+// Run pnpm icons:create to update
+// Do not edit
+import { SVGProps } from 'react';
+import { SVGRProps } from '../type';
+const SvgSandbox = ({
+    title,
+    titleId,
+    ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+    <svg
+        width="1em"
+        height="1em"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        data-testid="test-icon"
+        aria-hidden="true"
+        aria-labelledby={titleId}
+        {...props}
+    >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M10.502 1.403a3 3 0 0 1 2.998.001l3.194 1.825a1 1 0 0 1 .554.316L20.5 5.404c.37.214.69.503.937.848a1.004 1.004 0 0 1 .25.414A3 3 0 0 1 22 7.999v8.002a3 3 0 0 1-1.5 2.595l-.004.002-3.249 1.857a.998.998 0 0 1-.554.316L13.5 22.596l-.002.001a3.002 3.002 0 0 1-1.023.363.995.995 0 0 1-.95 0 3.001 3.001 0 0 1-1.023-.363l-.002-.001-3.193-1.825a.998.998 0 0 1-.554-.316l-3.249-1.857-.004-.002A3 3 0 0 1 2 16.001V8a3 3 0 0 1 .314-1.333 1.003 1.003 0 0 1 .25-.414 3 3 0 0 1 .936-.848l.004-.002 3.249-1.857a1 1 0 0 1 .553-.316l3.196-1.826zM7.55 5.393 5.04 6.828 12 10.855l6.961-4.027-2.51-1.435-3.95 2.283a1 1 0 0 1-1.001 0L7.55 5.393zm6.89-1.148L12.5 3.136a1 1 0 0 0-1 0l-.004.002L9.56 4.245 12 5.655l2.44-1.41zM20 8.538l-7 4.049v7.991l2.5-1.428V14.6a1 1 0 0 1 .5-.866l4-2.311V8.538zm0 5.195-2.5 1.444v2.83l2-1.143h.002a1 1 0 0 0 .498-.865v-2.266zm-9 6.845v-7.991l-7-4.05v2.886l4 2.311a1 1 0 0 1 .5.866v4.55l2.5 1.428zm-4.5-2.571v-2.83L4 13.733v2.266a1 1 0 0 0 .499.864l.001.001 2 1.143z"
+        />
+    </svg>
+);
+
+export default SvgSandbox;

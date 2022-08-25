1713335d5926b70306ffb89008e36618ccc76571
AbhinavMV <mvabhinav1998@gmail.com>
2022-08-26 00:18:08 +0530

    feat: Add web3uikit lib

---
+++ packages/web3uikit/src/index.ts
@@ -0,0 +1,6 @@
+export * from '@web3uikit/core';
+export * from '@web3uikit/icons';
+export * from '@web3uikit/styles';
+export * from '@web3uikit/web3';
+export { CreditCard as CreditCardIcon } from '@web3uikit/icons';
+export { CreditCard } from '@web3uikit/core';
---
+++ packages/web3uikit/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import config from './node_modules/@web3uikit/config/vite.config';
+
+export default defineConfig({
+    ...config(__dirname, [
+        'react',
+        'react-dom',
+        'react-router-dom',
+        'moralis',
+        'react-moralis',
+    ]),
+});

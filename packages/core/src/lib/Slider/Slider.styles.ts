7ba1f1d391c7727de197d498032c1658556aa79e
Hritwik Tripathi <52003051+3scava1i3r@users.noreply.github.com>
2022-09-04 23:06:21 +0530

    Update Slider.styles.ts

--- packages/core/src/lib/Slider/Slider.styles.ts
+++ packages/core/src/lib/Slider/Slider.styles.ts
@@ -1,10 +1,6 @@
 import styled, { css } from 'styled-components';
 import {color, fonts, resetCSS} from '@web3uikit/styles';
 
-// //////////////////
-// Styled Components
-// CSS Props should be sorted alphabetically
-// //////////////////
 
 
 const sliderDiv = styled.div`
@@ -38,16 +34,6 @@ const sliderInput = styled.input`
     width: 100%;
 `
 
-
-
-// CSS ORDERING
-// 1 - Imported styles (font, colors, etc)
-// 2 - Normal styles (margin, padding, etc)
-// 3 - Child elements (span, svg, etc)
-// 4 - Pseudo elements (before & after)
-// 5 - Modifier styles (hover, active etc)
-// 6 - State changed styles ${(p) => (p.prop ? green : red)};
-// each should be sorted alphabetically as best as possible
 
 export default {
     sliderDiv,

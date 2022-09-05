8af7e16dbc9fcdba234fc31ac522c0d7059a5b8e
3scava1i3r <tripathi.hritwik@gmail.com>
2022-09-05 20:18:31 +0530

    fixed crypto.com logo and added correct coinbase logo

---
+++ packages/core/src/lib/Illustrations/images/chains/cryptoweb.tsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import type { Size } from '../../types';
+
+const cryptoLogo = (width: Size = '120', height: Size = '160') => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={width}
+            height={height}
+            viewBox="0 0 120 160"
+            fill="none"
+        >
+            <g clipPath="url(#clip0_15291_165803)">
+                <path
+                    d="M109.8 109.4V51.8L59.9 23L10 51.8V109.4L59.9 138.2L109.8 109.4Z"
+                    fill="url(#paint0_linear_15291_165803)"
+                />
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M107.8 52.9549L59.9 25.3092L12 52.9549V108.245L59.9 135.891L107.8 108.245V52.9549ZM109.8 109.4V51.8L59.9 23L10 51.8V109.4L59.9 138.2L109.8 109.4Z"
+                    fill="white"
+                />
+                <path
+                    d="M78.9461 115H72.1437L64 107.39V103.488L72.4311 95.2927V82.3171L83.4491 75L96 84.6585L78.9461 115Z"
+                    fill="url(#paint1_linear_15291_165803)"
+                />
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M72.4311 95.2927L64 103.488V107.39L72.1437 115H78.9461L96 84.6585L83.4491 75L72.4311 82.3171V95.2927ZM74.4311 83.3897L83.3635 77.4577L93.4083 85.1877L77.776 113H72.9327L66 106.522V104.333L73.8251 96.7268C74.2126 96.3503 74.4311 95.833 74.4311 95.2927V83.3897Z"
+                    fill="white"
+                />
+                <path
+                    d="M50.8346 94L52.063 81.8692L48 71H72L68.0315 81.8692L69.1654 94H59.9055H50.8346Z"
+                    fill="url(#paint2_linear_15291_165803)"
+                />
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M52.063 81.8692L50.8346 94H69.1654L68.0315 81.8692L72 71H48L52.063 81.8692ZM50.8828 73L53.9364 81.1689C54.0439 81.4566 54.0838 81.7651 54.0528 82.0707L53.0474 92H66.9697L66.0402 82.0553C66.0126 81.7599 66.051 81.462 66.1528 81.1833L69.1406 73H50.8828Z"
+                    fill="white"
+                />
+                <path
+                    d="M56 107.293L47.9048 115H41.0476L24 84.561L36.5714 75L47.6191 82.2195V95.1951L56 103.39V107.293Z"
+                    fill="url(#paint3_linear_15291_165803)"
+                />
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M47.6191 95.1951V82.2195L36.5714 75L24 84.561L41.0476 115H47.9048L56 107.293V103.39L47.6191 95.1951ZM54 104.232L46.2208 96.6251C45.836 96.2488 45.6191 95.7333 45.6191 95.1951V83.3017L36.6588 77.4463L26.5945 85.1005L42.2198 113H47.1049L54 106.435V104.232Z"
+                    fill="white"
+                />
+                <path
+                    d="M40.4486 49H78.4547L83 68H36L40.4486 49Z"
+                    fill="url(#paint4_linear_15291_165803)"
+                />
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M42.0344 51L38.5224 66H80.4651L76.8768 51H42.0344ZM78.4547 49H40.4486L36 68H83L78.4547 49Z"
+                    fill="white"
+                />
+            </g>
+            <defs>
+                <linearGradient
+                    id="paint0_linear_15291_165803"
+                    x1="34.5045"
+                    y1="114.029"
+                    x2="128.952"
+                    y2="42.4689"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="white" stopOpacity="0.06" />
+                    <stop offset="1" stopColor="white" stopOpacity="0.85" />
+                </linearGradient>
+                <linearGradient
+                    id="paint1_linear_15291_165803"
+                    x1="71.8571"
+                    y1="106.607"
+                    x2="103.859"
+                    y2="84.2163"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="white" stopOpacity="0.06" />
+                    <stop offset="1" stopColor="white" stopOpacity="0.85" />
+                </linearGradient>
+                <linearGradient
+                    id="paint2_linear_15291_165803"
+                    x1="53.8929"
+                    y1="89.1741"
+                    x2="73.3987"
+                    y2="71.3729"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="white" stopOpacity="0.06" />
+                    <stop offset="1" stopColor="white" stopOpacity="0.85" />
+                </linearGradient>
+                <linearGradient
+                    id="paint3_linear_15291_165803"
+                    x1="31.8571"
+                    y1="106.607"
+                    x2="63.8594"
+                    y2="84.2163"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="white" stopOpacity="0.06" />
+                    <stop offset="1" stopColor="white" stopOpacity="0.85" />
+                </linearGradient>
+                <linearGradient
+                    id="paint4_linear_15291_165803"
+                    x1="47.5402"
+                    y1="64.0134"
+                    x2="59.8654"
+                    y2="37.3485"
+                    gradientUnits="userSpaceOnUse"
+                >
+                    <stop stopColor="white" stopOpacity="0.06" />
+                    <stop offset="1" stopColor="white" stopOpacity="0.85" />
+                </linearGradient>
+                <clipPath id="clip0_15291_165803">
+                    <rect
+                        width="99.8"
+                        height="115.2"
+                        fill="white"
+                        transform="translate(10 23)"
+                    />
+                </clipPath>
+            </defs>
+        </svg>
+    );
+};
+
+export default cryptoLogo;

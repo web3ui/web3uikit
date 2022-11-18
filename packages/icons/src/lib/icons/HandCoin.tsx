dadba0622de45ac34bbed5d838064252e876b279
Makda Sebahtu <sebhatumakda20gmail.com>
2022-11-18 15:37:45 +0100

    add two icons

---
+++ packages/icons/src/lib/icons/HandCoin.tsx
@@ -0,0 +1,28 @@
+// Auto-generated file created by svgr-cli source svg-template.js
+// Run pnpm icons:create to update
+// Do not edit
+import { SVGProps } from 'react';
+import { SVGRProps } from '../type';
+const SvgHandCoin = ({
+    title,
+    titleId,
+    ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+    <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        data-testid="test-icon"
+        aria-hidden="true"
+        aria-labelledby={titleId}
+        {...props}
+    >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path d="M16 12c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5Zm5.45 5.6c-.39-.4-.88-.6-1.45-.6h-7l-2.08-.73.33-.94L13 16h2.8c.35 0 .63-.14.86-.37.23-.23.34-.51.34-.82 0-.54-.26-.91-.78-1.12L8.95 11H7v9l7 2 8.03-3c.01-.53-.19-1-.58-1.4ZM5 11H.984v11H5V11Z" />
+    </svg>
+);
+
+export default SvgHandCoin;

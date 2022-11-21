b50d288309743ea7f1261b57588064a45edb8953
AbhinavMV <mvabhinav1998@gmail.com>
2022-11-21 10:50:42 +0530

    chore(Icons): add new icons

---
+++ packages/icons/src/lib/icons/SushiSwap.tsx
@@ -0,0 +1,93 @@
+// Auto-generated file created by svgr-cli source svg-template.js
+// Run pnpm icons:create to update
+// Do not edit
+import { SVGProps } from 'react';
+import { SVGRProps } from '../type';
+const SvgSushiSwap = ({
+    title,
+    titleId,
+    ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+    <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 49 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        data-testid="test-icon"
+        aria-hidden="true"
+        aria-labelledby={titleId}
+        {...props}
+    >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <g clipPath="url(#sushi-swap_svg__a)">
+            <path
+                d="m10.3 4.598 37.2 25.4-9.2 13.6-37.2-25.6 9.2-13.4Z"
+                fill="url(#sushi-swap_svg__b)"
+            />
+            <path
+                d="M47.5 29.998c-3.2 4.6-14 2.8-24.2-4.4-10.4-7-16-16.4-13-21 3.2-4.6 14-2.8 24.2 4.4 10.4 6.8 16 16.4 13 21Z"
+                fill="url(#sushi-swap_svg__c)"
+            />
+            <path
+                d="M38.3 43.398c-3.2 4.6-14 2.8-24.2-4.4s-16-16.4-12.8-21.2c3.2-4.6 14-2.8 24.2 4.4s15.8 16.6 12.8 21.2Z"
+                fill="url(#sushi-swap_svg__d)"
+            />
+            <path
+                d="m47.5 30-9.2 13.6c-3.2 4.6-14 2.6-24.2-4.4-2-1.4-3.8-2.8-5.6-4.4 1.4-.2 3.2-1 5-3 3.2-3.4 4.8-4.2 6.2-4 1.4 0 3 1.4 5.6 4.8 2.6 3.4 6.2 4.4 8.4 2.6.2-.2.4-.2.6-.4 1.8-1.4 2.4-2 5.8-8.4.8-1.6 3.6-4.2 7.4-3 1 2.6 1 4.8 0 6.6Z"
+                fill="#0E0F23"
+            />
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M46.1 29.198c-2.8 4-12.6 2-22-4.6-9.6-6.6-15.2-15-12.4-19 2.8-4 12.6-2 22 4.6s15 15 12.4 19Zm-8.8-6c-1.4 2-6.2 1-11-2.2-4.6-3.2-7.4-7.4-6-9.4 1.4-2 6.2-1 11 2.2 4.6 3.2 7.4 7.4 6 9.4Z"
+                fill="#fff"
+            />
+            <path
+                d="M9.5 9.197c0-.2-.2-.4-.4-.2s-.4.2-.4.4c.2.6.4 1 .4 1.4 0 .2.2.4.4.2.2 0 .4-.2.2-.4 0-.4 0-.8-.2-1.4ZM10.5 12.4c0-.2-.2-.4-.4-.2s-.2.2-.2.4c2.2 5 6.8 10.4 12.8 14.4.2.2.4 0 .6 0 .2-.2 0-.4 0-.6-6.2-4-10.6-9.2-12.8-14ZM34.7 32c-.2 0-.4 0-.4.2s0 .4.2.4c.6.2 1.4.4 2 .6.2 0 .4 0 .4-.2s0-.4-.2-.4c-.6-.2-1.4-.4-2-.6ZM38.3 32.797c-.2 0-.4.2-.4.4s.2.4.4.4c1.6.2 3.4.4 4.8.2.2 0 .4-.2.4-.4s-.2-.4-.4-.4c-1.6.2-3.2 0-4.8-.2Z"
+                fill="#fff"
+            />
+        </g>
+        <defs>
+            <linearGradient
+                id="sushi-swap_svg__b"
+                x1={17.588}
+                y1={0.986}
+                x2={24.965}
+                y2={26.319}
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#03B8FF" />
+                <stop offset={1} stopColor="#FA52A0" />
+            </linearGradient>
+            <linearGradient
+                id="sushi-swap_svg__c"
+                x1={23.664}
+                y1={-0.782}
+                x2={31.041}
+                y2={24.55}
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#03B8FF" />
+                <stop offset={1} stopColor="#FA52A0" />
+            </linearGradient>
+            <linearGradient
+                id="sushi-swap_svg__d"
+                x1={11.532}
+                y1={2.75}
+                x2={18.91}
+                y2={28.082}
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#03B8FF" />
+                <stop offset={1} stopColor="#FA52A0" />
+            </linearGradient>
+            <clipPath id="sushi-swap_svg__a">
+                <path fill="#fff" transform="translate(.3)" d="M0 0h48v48H0z" />
+            </clipPath>
+        </defs>
+    </svg>
+);
+
+export default SvgSushiSwap;

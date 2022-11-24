18cb3c40388e830703cc5aee274f9f96c0d64009
0xTijan <tijan.nartnik@gmail.com>
2022-11-24 16:55:05 +0100

    [Icon]: added heart icon

---
+++ packages/icons/src/lib/icons/Heart.tsx
@@ -0,0 +1,30 @@
+// Auto-generated file created by svgr-cli source svg-template.js
+// Run pnpm icons:create to update
+// Do not edit
+import { SVGProps } from 'react';
+import { SVGRProps } from '../type';
+const SvgHeart = ({
+    title,
+    titleId,
+    ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+    <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 25"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        data-testid="test-icon"
+        aria-hidden="true"
+        aria-labelledby={titleId}
+        {...props}
+    >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+            d="M12.002 22.91c-.26 0-.51-.1-.71-.29l-8.84-8.84a6.525 6.525 0 0 1 0-9.2 6.524 6.524 0 0 1 9.2 0l.35.35.35-.35a6.503 6.503 0 0 1 9.21.02c2.52 2.54 2.51 6.65-.02 9.17l-8.84 8.84c-.2.2-.45.29-.71.29l.01.01ZM7.052 4.68c-1.15 0-2.31.44-3.18 1.32-1.75 1.75-1.75 4.61 0 6.37l8.13 8.13 8.13-8.13c1.75-1.75 1.76-4.61 0-6.36s-4.6-1.74-6.36 0l-1.06 1.06c-.38.38-1.04.38-1.41 0l-1.06-1.06a4.483 4.483 0 0 0-3.18-1.32l-.01-.01Z"
+            fill="#68738D"
+        />
+    </svg>
+);
+export default SvgHeart;

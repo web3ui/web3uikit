299fa93a79bff699114f3d7e08c584a033a1de55
Kristam Moffett <hello@kristam-moffett.com>
2022-12-14 13:27:55 +0000

    chore: multiple icons added

---
+++ packages/icons/src/lib/icons/Layout.tsx
@@ -0,0 +1,30 @@
+// Auto-generated file created by svgr-cli source svg-template.js
+// Run pnpm icons:create to update
+// Do not edit
+import { SVGProps } from 'react';
+import { SVGRProps } from '../type';
+const SvgLayout = ({
+    title,
+    titleId,
+    ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+    <svg
+        aria-hidden="true"
+        data-testid="test-icon"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        role="img"
+        aria-labelledby={titleId}
+        {...props}
+    >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path fillRule="evenodd" clipRule="evenodd" d="M5 4C4.44771 4 4 4.44771 4 5V19C4 19.5523 4.44771 20 5 20H19C19.5523 20 20 19.5523 20 19V5C20 4.44771 19.5523 4 19 4H5ZM2 5C2 3.34315 3.34315 2 5 2H19C20.6569 2 22 3.34315 22 5V19C22 20.6569 20.6569 22 19 22H5C3.34315 22 2 20.6569 2 19V5Z" fill="black" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M2 9C2 8.44772 2.44772 8 3 8H21C21.5523 8 22 8.44772 22 9C22 9.55228 21.5523 10 21 10H3C2.44772 10 2 9.55228 2 9Z" fill="black" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M9 8C9.55228 8 10 8.44772 10 9V21C10 21.5523 9.55228 22 9 22C8.44772 22 8 21.5523 8 21V9C8 8.44772 8.44772 8 9 8Z" fill="black" />
+    </svg>
+);
+
+export default SvgLayout;

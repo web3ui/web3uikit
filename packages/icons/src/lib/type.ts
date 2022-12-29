66a4c76a43a630baadcb57ca5fc2278ca5f1c264
AbhinavMV <mvabhinav1998@gmail.com>
2022-12-29 13:01:42 +0530

    feat:add flex none to icons create template

--- packages/icons/src/lib/type.ts
+++ packages/icons/src/lib/type.ts
@@ -1,7 +1,10 @@
 import { SVGProps } from 'react';
+
 export interface SVGRProps {
     title?: string;
     titleId?: string;
+    isResponsive?: boolean;
+    style?: SVGSVGElement['style'];
 }
 
-export type TIconType = SVGProps<SVGSVGElement> & SVGRProps;
+export type TIconType = SVGProps<Omit<SVGSVGElement, 'style'>> & SVGRProps;

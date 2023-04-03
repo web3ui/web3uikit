9b94b8158572ea302c325c2160f9603a4f783ba2
Kristam Moffett <kristam@moralis.io>
2023-04-03 08:23:49 +0100

    Added gmail, etherscan, slack and learn icons

---
+++ packages/icons/src/lib/icons/Slack.tsx
@@ -0,0 +1,53 @@
+// Auto-generated file created by svgr-cli source svg-template.js
+// Run pnpm icons:create to update
+// Do not edit
+import { SVGProps } from 'react';
+import { SVGRProps } from '../type';
+const SvgSlack = ({
+    title,
+    titleId,
+    isResponsive = false,
+    style,
+    ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+    <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        data-testid="test-icon"
+        aria-hidden="true"
+        style={
+            isResponsive
+                ? style
+                : {
+                      flex: 'none',
+                      ...style,
+                  }
+        }
+        aria-labelledby={titleId}
+        {...props}
+    >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+            d="M6.558 14.217a1.92 1.92 0 0 1-1.921 1.921 1.92 1.92 0 0 1-1.922-1.921 1.92 1.92 0 0 1 1.922-1.922h1.921v1.922Zm.961 0a1.92 1.92 0 0 1 1.922-1.922 1.92 1.92 0 0 1 1.922 1.922v4.804a1.92 1.92 0 0 1-1.922 1.922 1.92 1.92 0 0 1-1.922-1.922v-4.804Z"
+            fill="#E01E5A"
+        />
+        <path
+            d="M9.44 6.5A1.92 1.92 0 0 1 7.52 4.58a1.92 1.92 0 0 1 1.92-1.922 1.92 1.92 0 0 1 1.923 1.922v1.922H9.44Zm0 .976a1.92 1.92 0 0 1 1.923 1.922A1.92 1.92 0 0 1 9.44 11.32H4.62a1.92 1.92 0 0 1-1.92-1.922 1.92 1.92 0 0 1 1.92-1.922h4.82Z"
+            fill="#36C5F0"
+        />
+        <path
+            d="M17.142 9.398a1.92 1.92 0 0 1 1.922-1.922 1.92 1.92 0 0 1 1.922 1.922 1.92 1.92 0 0 1-1.922 1.922h-1.922V9.398Zm-.96 0a1.92 1.92 0 0 1-1.922 1.922 1.92 1.92 0 0 1-1.922-1.922V4.579a1.92 1.92 0 0 1 1.922-1.922A1.92 1.92 0 0 1 16.18 4.58v4.819Z"
+            fill="#2EB67D"
+        />
+        <path
+            d="M14.26 17.1a1.92 1.92 0 0 1 1.921 1.921 1.92 1.92 0 0 1-1.921 1.922 1.92 1.92 0 0 1-1.922-1.922v-1.922h1.922Zm0-.962a1.92 1.92 0 0 1-1.922-1.921 1.92 1.92 0 0 1 1.922-1.922h4.818A1.92 1.92 0 0 1 21 14.217a1.92 1.92 0 0 1-1.922 1.921H14.26Z"
+            fill="#ECB22E"
+        />
+    </svg>
+);
+
+export default SvgSlack;

fc7bace33a979cfa1b628eb9ddb59858708f1283
Vadym Beletskyi <mrdexters1@gmail.com>
2023-06-07 09:46:48 +0200

    refactor description updated

--- packages/core/src/lib/CryptoAddress/types.ts
+++ packages/core/src/lib/CryptoAddress/types.ts
@@ -3,9 +3,6 @@ import { variantType, TStandardWeight } from '../Typography';
 export interface ICryptoAddressProps {
     typographyFontWeight?: TStandardWeight;
     typographyVariant?: variantType;
-    /**
-     * text to display to the users, should be some sort of status
-     */
     walletAddress: string;
     walletColor?: string;
     walletUrl?: string;

8b48a7229ac4206635b1993dd22594f8d7a7f7d4
dani <martinelli.daniele7@gmail.com>
2023-07-01 16:52:42 +0200

    gas icon

---
+++ packages/icons/src/lib/icons/Gas.tsx
@@ -0,0 +1,58 @@
+// Auto-generated file created by svgr-cli source svg-template.js
+// Run pnpm icons:create to update
+// Do not edit
+import { SVGProps } from 'react';
+import { SVGRProps } from '../type';
+const SvgGas = ({
+    title,
+    titleId,
+    isResponsive = false,
+    style,
+    ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+    <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        data-testid="test-icon"
+        aria-hidden="true"
+        style={
+            isResponsive
+                ? style
+                : {
+                      flex: 'none',
+                      ...style,
+                  }
+        }
+        aria-labelledby={titleId}
+        {...props}
+    >
+        {title ? <title id={titleId}>{title}</title> : null}
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M4.574 1.906a.26.26 0 0 0-.259.26V14.61c0 .143.116.259.26.259h10.002a.26.26 0 0 0 .26-.26V2.165a.26.26 0 0 0-.26-.259H4.574ZM15.926 15.89l.579.58H4.575a1.86 1.86 0 0 1-1.86-1.858V2.165A1.86 1.86 0 0 1 4.575.306h10.002a1.86 1.86 0 0 1 1.86 1.86V14.61c0 .496-.195.946-.51 1.28Z"
+        />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M4.315 14.33v7.505c0 .143.116.26.26.26h10.002a.259.259 0 0 0 .26-.26V14.33h1.6v7.505c0 1.027-.833 1.86-1.86 1.86H4.574a1.86 1.86 0 0 1-1.859-1.86V14.33h1.6Z"
+        />
+        <path d="M7.21 3.986h4.732c.35 0 .634.284.634.634v4.732c0 .35-.284.634-.634.634H7.21a.634.634 0 0 1-.634-.634V4.62c0-.35.283-.634.634-.634Z" />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M7.376 4.786v4.4h4.4v-4.4h-4.4Zm-1.6-.166c0-.792.642-1.434 1.434-1.434h4.732c.792 0 1.434.642 1.434 1.434v4.732c0 .792-.642 1.434-1.434 1.434H7.21a1.434 1.434 0 0 1-1.434-1.434V4.62ZM16.905 3.53a.8.8 0 0 1 .8-.8h1.962a2.624 2.624 0 0 1 2.618 2.619v12.506c0 1.13-.932 2.042-2.068 2.011a1.867 1.867 0 0 1-1.364-.626c-.32-.357-.497-.81-.578-1.26l-1.014-4.882v-.082a.412.412 0 0 0-.412-.412h-1.213v-1.6h1.213c1.08 0 1.962.851 2.01 1.92l.988 4.755.002.013c.044.25.126.402.195.479a.27.27 0 0 0 .217.096.412.412 0 0 0 .424-.412V5.349c0-.558-.46-1.018-1.018-1.018h-1.962a.8.8 0 0 1-.8-.8Z"
+        />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M18.26 6.38V3.53h1.6v2.85a1.2 1.2 0 0 0 1.2 1.2h.425v1.6h-.424a2.8 2.8 0 0 1-2.8-2.8Z"
+        />
+    </svg>
+);
+
+export default SvgGas;
